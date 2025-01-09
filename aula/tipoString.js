e062315d294dfff5da271687f7a2f7e0d2ebad7a
devluish <luis_unn@hotmail.com>
2025-01-09 18:44:23 -0300

    commit

---
+++ aula/tipoString.js
@@ -0,0 +1,10 @@
+const estudante = "Luis";
+const docente = "Ana";
+const cumprimento = "Nosso lema é 'estudar bastante!'";
+const citacao = `Ju diz: "Nosso lema é 'estudar bastante!'"`;
+
+
+console.log(cumprimento);
+console.log(citacao);
+
+console.log(`O estudante chama ${estudante}`);
---
+++ aula/tiposNumbers.js
@@ -0,0 +1,12 @@
+const notaPrimeiroBi = 8;
+const notaSegundoBi = 6.3;
+const notaTerceiroBi = -2;
+const notaQuartoBi = Number.parseInt('5'); //Fazendo a conversão de valores de string para inteiro.
+
+const total = (notaPrimeiroBi + notaSegundoBi + notaTerceiroBi + notaQuartoBi) / 4;
+
+console.log(total);
+
+const nome = "Luis";
+console.log("A media é " + total.toFixed(2)); //Tofixed serve para ajustar as casas decimais de acordo com o valor desajado.
+
