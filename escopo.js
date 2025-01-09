dea1a0a64f9e360b7d53a3e3efd032e6f1bc4cb7
devluish <luis_unn@hotmail.com>
2025-01-09 14:57:23 -0300

    commit

---
+++ escopo.js
@@ -0,0 +1,9 @@
+if(1 > 0) { //Primeiro entrou dentro do if, pois a condição é true
+    const estudante = 'Caroline'; //Criou a variável estudante com Caroline
+    console.log(estudante); //Imprimiu o valor do console, pois primeiro atribuiu a variável dentro do bloco IF
+}
+
+estudante = 'Ana'; //Mudou o nome da variável para Ana
+console.log(estudante); //Fez a impressão fora do bloco, atribuiu um novo valor para estudante
+
+//O estilo de variável LET é recomendado em casos unicos, ela pode ser alterada a qualquer momente, inclusive dentro de blocos.
