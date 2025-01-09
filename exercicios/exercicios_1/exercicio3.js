04d76ac34b4c958fdf1465257619d908795e3146
devluish <luis_unn@hotmail.com>
2025-01-09 18:17:32 -0300

    commit

---
+++ exercicios/exercicios_1/exercicio3.js
@@ -0,0 +1,11 @@
+/*
+Declare duas variáveis, uma contendo um número e outra contendo uma string. 
+Em seguida, combine-as em uma terceira variável usando template strings para montar uma frase e exiba o resultado no console.
+*/
+
+const nome = "Luis";
+const idade = 25;
+
+
+const frase = `A idade de ${nome} é de ${idade} anos`;
+console.log(frase);  
---
+++ exercicios/exercicios_1/exercicio4.js
@@ -0,0 +1,11 @@
+/**
+ Crie uma variável inicializada com um valor de qualquer tipo e, em seguida, reatribua um novo valor a essa variável.
+ Imprima ambos os valores no console após atribuí-los.
+ */
+
+ let nome = "luis";
+ console.log(nome);
+
+ nome =  "Luis Henrique";
+ console.log(nome);
+
