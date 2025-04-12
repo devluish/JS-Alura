5fa3d22b44f52a1523a624677609a4bbc2dc846e
devluish <luis_unn@hotmail.com>
2025-04-12 13:01:05 -0300

    commit

---
+++ revisao/ExerciciosChatGptRevisao/tipoString.js
@@ -0,0 +1,6 @@
+
+// Crie uma string e imprima seu tamanho usando .length.
+
+const nomeCompleto = "Luis Henrique Oliveira";
+
+console.log(`Nome:${nomeCompleto} Tamanho:${nomeCompleto.length}`);
--- revisao/ExerciciosChatGptRevisao/variaveis.js
+++ revisao/ExerciciosChatGptRevisao/variaveis.js
@@ -1,4 +1,7 @@
-//Crie três variáveis: uma com seu nome, uma com sua idade e uma dizendo se você estuda JavaScript (true/false).
+//1- Crie três variáveis: uma com seu nome, uma com sua idade e uma dizendo se você estuda JavaScript (true/false).
+//2- Altere o valor da variável da sua idade.
+//3- Crie uma constante com o nome da sua cidade e tente reatribuir (observe o erro).
+
 
 //Nome
 const nome = "Luis Henrique";
@@ -9,4 +12,15 @@ let idade = 25;
 // boolean
 const estudaProgramacao = true;
 
-console.log(`nome: ${nome}, idade: ${idade}, estuda programação? ${estudaProgramacao}`);
+//console.log(`nome: ${nome}, idade: ${idade}, estuda programação? ${estudaProgramacao}`);
+
+idade = 26;
+
+// console.log(idade);
+
+const cidade = "São Paulo";
+
+console.log(cidade);
+
+// cidade = "Rio de Janeiro" -> Reatribuindo o valor da const - Isso resulta em erro Assignment to constant variable 
+
