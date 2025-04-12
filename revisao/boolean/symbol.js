b882c18c79c1179a7f10a3c7d5a67c234e3ad1d2
devluish <luis_unn@hotmail.com>
2025-04-12 16:08:49 -0300

    commit

---
+++ revisao/boolean/symbol.js
@@ -0,0 +1,9 @@
+// Crie dois Symbols e teste se eles são iguais.
+const numero = Symbol("1");
+
+const numero2 = Symbol("1");
+
+console.log(numero === numero2);
+
+
+
---
+++ revisao/boolean/tipoNull-Undefined.js
@@ -0,0 +1,11 @@
+// Crie uma variável booleana que só é true se um número for maior que 18.
+const idade = 20;
+const maiorIdade = (idade >= 18);
+
+
+if (maiorIdade){
+    console.log("Maior de idade");
+} else{
+    console.log("Menor de idade");
+    
+}
