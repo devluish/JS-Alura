47e8985bf85425675fc9ab29f191302a74e4ad1a
devluish <luis_unn@hotmail.com>
2025-04-12 13:41:20 -0300

    commit

---
+++ revisao/boolean/tipoBoolean.js
@@ -0,0 +1,15 @@
+// Converta um número em string e vice-versa.
+
+let numero = 25;
+
+let numeroString = numero.toString();
+
+
+
+console.log('Numero como string: ' + numeroString);
+
+// Convertendo de volta
+
+let StringNumero = Number(numeroString);
+
+console.log('String como numero: ' + StringNumero);
---
+++ revisao/boolean/tiposNumbers.js
@@ -0,0 +1,11 @@
+// Verifique o tipo de null e undefined com typeof.
+
+//undefined pois não foi atribuido a nada
+let nome;
+//normal pois foi atribuido uma String a variável
+let sobreNome = "Henrique";
+//valor atribuido como null
+let idade = null;
+
+console.log(typeof nome);
+console.log(typeof idade);
