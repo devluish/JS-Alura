ff4262ec276737396fd5b4865be634c283a33e2e
devluish <luis_unn@hotmail.com>
2025-04-13 14:16:03 -0300

    commit

---
+++ revisao/funções/funcaoNome.js
@@ -0,0 +1,11 @@
+//Crie uma função que imprime o seu nome em letras maiúsculas.'
+
+const nome = () => "luis"
+console.log(toUpperCase(nome()));
+
+
+/**Feito pelo IA dúvida -> Forma lógica */
+const imprimirNomeMaiusculo = (nome) => {
+    console.log(nome.toUpperCase("luis"));
+  };
+  imprimirNomeMaiusculo("luis");
