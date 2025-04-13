f1479fffbd5babffd19c984c93a7c28841b5bbe2
devluish <luis_unn@hotmail.com>
2025-04-13 18:09:41 -0300

    commit

--- revisao/DESAFIO/funcao.js
+++ revisao/DESAFIO/funcao.js
@@ -3,3 +3,24 @@
 // "buzz" se for divisível por 5
 // "fizzbuzz" se for divisível por ambos
 
+
+//Primeira tentativa
+// const numero = 15;
+
+// if(numero % 3 === 0 && numero % 5 === 0){
+//     console.log("FIZZ BUZZ");    
+
+// }else if(numero % 3 === 0){
+//     console.log("FIZZ");
+    
+// }else if(numero % 5 === 0){
+//     console.log("BUZZ");
+    
+// }
+
+
+const numero = 10;
+
+
+
+
