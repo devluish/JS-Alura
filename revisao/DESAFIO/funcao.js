65fac66639c3529c29ab538b0975da8b04ef66a8
devluish <luis_unn@hotmail.com>
2025-04-13 22:47:34 -0300

    Finalizando exercicios

--- revisao/DESAFIO/funcao.js
+++ revisao/DESAFIO/funcao.js
@@ -19,7 +19,6 @@
 // }
 
 
-const numero = 10;
 
 
 
--- revisao/DESAFIO/site.js
+++ revisao/DESAFIO/site.js
@@ -3,3 +3,46 @@
 // Ter aceitado os termos (true)
 // E estar logado (true)
 
+
+// segunda tentativa
+// const idade = 18;
+// const termos = true;
+// const login = true;
+
+
+// const verificacao = (idade, termos, login) => {
+//     if(idade >= 18){
+//         console.log("Usuário maior de idade, pode acessar");   
+//     }else{
+//         console.log("Menor de idade");
+        
+//     }if(termos){
+//         console.log("Termos aceitos");
+//     }else{
+//         console.log("Precisa aceitar os termos antes de prosseguir");
+//     }
+//     if(login ){
+//         console.log("Feito o Login com sucesso!");
+//     }else{
+//         console.log("Faz o L");
+        
+//     }
+// }
+
+// verificacao(idade, termos, login);
+
+// terceira
+
+const idade = 18;
+const termos = true;
+const login = true;
+
+
+const verificacao = (idade, termos, login) => {
+    if(idade >= 18 && termos && login){
+        console.log("Usuário pode acessar o sistema!");
+        
+    }
+}
+
+verificacao(idade, termos, login);
