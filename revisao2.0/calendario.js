eae8a30746073712808a416e8aa797a1b17aa053
devluish <luis_unn@hotmail.com>
2025-04-15 22:44:03 -0300

    Commit

--- revisao2.0/calendario.js
+++ revisao2.0/calendario.js
@@ -8,3 +8,89 @@
 // ...
 
 // 7 → Sábado
+
+//PRIMEIRA TENTATIVA
+// const diaSemana = (domingo, segunda, terca, quarta, quinta, sexta, sabado) => {
+
+//     if(1 === domingo){
+//         console.log("Domingo");
+        
+//     }else if(2 === segunda){
+//         console.log("Segunda");
+        
+//     }else if(3 === terca){
+//         console.log("Terça");
+        
+//     }else if(4 === quarta){
+//         console.log("Quarta");
+        
+//     }else if(5 === quinta){
+//         console.log("Quinta");
+        
+//     }else if(6 === sexta){
+//         console.log("Sexta");
+        
+//     }else if(7 === sabado){
+//         console.log("Sabado");
+        
+//     } else{
+//         console.log("Digite um valor de 1 a 7");
+        
+//     }
+
+// }
+
+
+// diaSemana(1, 2, 3, 4, 5, 6, 7);
+
+
+//SEGUNDA TENTATIVA:
+// const diaSemana = (domingo, segunda, terca, quarta, quinta, sexta, sabado) => {
+//     console.log(`${domingo}- Domingo`);
+//     console.log(`${segunda}- Segunda`);
+//     console.log(`${terca}- Terça`);
+//     console.log(`${quarta}- Quarta`);
+//     console.log(`${quinta}- Quinta`);
+//     console.log(`${sexta}- Sexta`);
+//     console.log(`${sabado}- Sabado`);
+    
+// }
+
+
+// diaSemana(1, 2, 3, 4, 5, 6, 7);
+
+
+
+//TERCEIRA TENTATIVA
+
+function diaSemana (dia) {
+    if (dia === 1) {
+        return "Domingo"
+        
+    }else if(dia === 2){
+        return "Segunda"
+    
+    }else if (dia === 3) {
+        return "Terça"
+        
+    }else if(dia === 4){
+        return "Quarta"
+    
+    }else if(dia === 5){
+        return "Quinta"
+    
+    }else if(dia === 6){
+        return "Sexta"
+    
+    }else if(dia === 7){
+        return "Sabado"
+    }
+}
+
+console.log(`1-${diaSemana(1)}`);
+console.log(`1-${diaSemana(2)}`);
+console.log(`1-${diaSemana(3)}`);
+console.log(`1-${diaSemana(4)}`);
+console.log(`1-${diaSemana(5)}`);
+console.log(`1-${diaSemana(6)}`);
+console.log(`1-${diaSemana(7)}`);
--- revisao2.0/compras.js
+++ revisao2.0/compras.js
@@ -6,3 +6,22 @@
 // Se for acima de R$200, dê 20%
 
 // Mostre o valor com o desconto aplicado
+
+const compras = (valor) => {
+
+    console.log(`Valor original: R$${valor}`);
+    
+    if(valor > 200){
+        const desconto20 = valor - 20 / 100 * valor;
+        console.log(`Valor da compra com desconto 1: ${desconto20}`);
+
+    } else if(valor > 100){
+        const desconto10 = valor - 10 / 100 * valor;
+        console.log(`Valor da compra com desconto 2: ${desconto10}`);
+        
+    } else if(valor < 100){
+        console.log("Não tem desconto");
+    }
+}
+
+compras(38);
